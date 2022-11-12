f1d3ce2297f2dcd6a5b8110ae14c4f0c3f94f5e0
drew <58788781+drew138@users.noreply.github.com>
2022-11-12 12:49:12 -0500

    fixed symbolic links script

--- scripts/simbolic_links.sh
+++ scripts/simbolic_links.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-DEV="$HOME/dev"
 FONTS_DIR="$HOME/.local/share/fonts"
 CONFIG_DIR="$HOME/.config"
 # create symbolic links
 # sfv (symbolic force verbose) for idempotency
 # files
-ln -sfv "$DEV/.dotfiles/.gitconfig" "$HOME/.gitconfig"  
-ln -sfv "$DEV/.dotfiles/.zshrc" "$HOME/.zshrc"
-ln -sfv "$DEV/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -sfv "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"  
+ln -sfv "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+ln -sfv "$HOME/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 # dirs
-ln -sfv "$DEV/.dotfiles/desktop/fonts/NerdFonts" $FONTS_DIR
-ln -sfv "$DEV/.dotfiles/.config/nvim" $CONFIG_DIR
-ln -sfv "$DEV/.dotfiles/.config/kitty" $CONFIG_DIR
-ln -sfv "$DEV/.dotfiles/.config/coc/ultisnips" "$CONFIG_DIR/coc"
+ln -sfv "$HOME/.dotfiles/desktop/fonts/NerdFonts" $FONTS_DIR
+ln -sfv "$HOME/.dotfiles/.config/nvim" $CONFIG_DIR
+ln -sfv "$HOME/.dotfiles/.config/kitty" $CONFIG_DIR
+ln -sfv "$HOME/.dotfiles/.config/coc/ultisnips" "$CONFIG_DIR/coc"
 # programs
 ln -sfv $(which fdfind) ~/.local/bin/fd
