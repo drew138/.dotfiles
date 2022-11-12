99f654ddd80c11633db2a817c5d7135598439bb3
drew <58788781+drew138@users.noreply.github.com>
2022-11-12 10:10:52 -0500

    test

--- scripts/ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# ansible
-sudo apt-get install ansible
-sudo ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# TODO check ssh connection is workign
+ssh -T git@github.com
+
 DIR="$HOME/.dotfiles"
-if [ ! -d "$DIR" ];
-then 
-    git clone "git@github.com:Drew138/.dotfiles.git" "$DIR"
-fi
 
 SCRIPT_DIR="$DIR/scripts"
-source "$SCRIPT_DIR/ansible.sh"
+
+sudo apt-get install ansible
+sudo ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml
+
 source "$SCRIPT_DIR/simbolic_links.sh"
 source "$SCRIPT_DIR/python_venv.sh"
 source "$SCRIPT_DIR/nvim.sh"
