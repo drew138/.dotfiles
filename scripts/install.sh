ae0ef22993fe16fd1eb5047d1c0ab566bae48766
drew <58788781+drew138@users.noreply.github.com>
2022-11-14 11:07:47 -0500

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$DIR/scripts"
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 sudo ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml
 
 "$SCRIPT_DIR/simbolic_links.sh"
