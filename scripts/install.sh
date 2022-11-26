cdd60b758d1685f8e430ad52b57b26f9453bf82c
drew <58788781+drew138@users.noreply.github.com>
2022-11-25 23:02:03 -0500

    changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,5 @@ SCRIPT_DIR="$DIR/scripts"
 # sudo apt-get install ansible -y
 sudo ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml
 
-"$SCRIPT_DIR/simbolic_links.sh"
-"$SCRIPT_DIR/python_venv.sh"
+# "$SCRIPT_DIR/simbolic_links.sh"
+# "$SCRIPT_DIR/python_venv.sh"
