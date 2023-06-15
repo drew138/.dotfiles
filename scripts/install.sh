9fddd0b67d100ee392185d7ad6f2af4694d4e413
drew <58788781+drew138@users.noreply.github.com>
2023-06-14 21:55:23 -0500

    testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-DIR="$HOME/.dotfiles"
-
-SCRIPT_DIR="$DIR/scripts"
-
 install(){
   sudo ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml \
       --extra-vars \
       "user_repositories=$USER_REPOSITORIES \
       kitty_terminal=$KITTY_TERMINAL \
       extra_software=$EXTRA_SOFTWARE"
-
-  "$SCRIPT_DIR/simbolic_links.sh"
-  "$SCRIPT_DIR/python_venv.sh"
 }
 
 package="install.sh"
