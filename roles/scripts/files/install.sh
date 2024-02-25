1b67314bc338a3178efea6301bfb1c484f7cdf0d
drew <58788781+drew138@users.noreply.github.com>
2024-02-25 13:04:24 -0500

    feat: wip

--- roles/scripts/files/install.sh
+++ roles/scripts/files/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo -n "Please enter your vault password: "
-
-read -r -s password
+read -p "Please enter your vault password: " -r -s password
 
 echo "$password" > ~/.vault_pass
 
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass
 
-ansible-pull -U https://github.com/Drew138/.dotfiles.git ansible/local.yml
+ansible-pull -U https://github.com/Drew138/.dotfiles.git local.yml
 
 rm ~/.vault_pass
 
