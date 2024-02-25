9bcb952bc81cd1503bf3e374e02e03dd95bf2fe9
drew <58788781+drew138@users.noreply.github.com>
2024-02-25 13:44:03 -0500

    feat: wip

--- roles/scripts/files/install.sh
+++ roles/scripts/files/install.sh
@@ -6,7 +6,7 @@ echo "$password" > ~/.vault_pass
 
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass
 
-ansible-pull -U https://github.com/Drew138/.dotfiles.git local.yml
+ansible-pull -U https://github.com/Drew138/.dotfiles.git local.yml --extra-vars selected_roles=go
 
 rm ~/.vault_pass
 
