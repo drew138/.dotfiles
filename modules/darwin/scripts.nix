41f4ab203f71256fd5f88c0cc25722ca2a5a621f
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-02 16:36:59 -0500

    feat: wip

--- modules/darwin/scripts.nix
+++ modules/darwin/scripts.nix
@@ -1,14 +1,26 @@
 { pkgs, config, ... }: {
 
   system.activationScripts = {
-    setupProgrammingLanguage.text = pkgs.lib.mkForce ''
-      echo "settings up python..." >&2
+    languages.text = pkgs.lib.mkForce ''
+      echo "Setting up Python with pyenv..." >&2
+
       export PYENV_ROOT="$HOME/.pyenv"
       export PATH="$PYENV_ROOT/bin:$PATH"
       eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
-      version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1)
-      ${pkgs.pyenv}/bin/pyenv install --skip-existing $version
-      ${pkgs.pyenv}/bin/pyenv global $version
+
+      # Get latest Python 3 version
+      version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
+
+      if [[ -z "$version" ]]; then
+        echo "Failed to determine the latest Python version." >&2
+        exit 1
+      fi
+
+      echo "Installing Python $version..." >&2
+      ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
+      ${pkgs.pyenv}/bin/pyenv global "$version"
+
+      echo "Python $(python --version) is now active."
     '';
 
     applications.text =
