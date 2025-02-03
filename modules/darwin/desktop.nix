3ad013b99453bbfeaccf3d8250e5d395a695ff37
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-02 20:26:58 -0500

    fix: wip

--- modules/darwin/desktop.nix
+++ modules/darwin/desktop.nix
@@ -10,10 +10,7 @@
       show-recents = false;
       tilesize = 36;
       persistent-apps = [
-        {
-          path = "/Applications/System Settings.app";
-          pos = 1;
-        }
+        "/Applications/System_Settings.app"
         "${pkgs.postman}/Applications/Postman.app"
         # "${pkgs.bitwarden-desktop}/Applications/Bitwarden.app"
         "${pkgs.wezterm}/Applications/WezTerm.app"
