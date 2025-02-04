309d5f509462c8ec715abfbe7982bd0e36e54716
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-04 15:33:29 -0500

    feat: wip

--- modules/darwin/scripts.nix
+++ modules/darwin/scripts.nix
@@ -23,70 +23,50 @@
     #   echo "Python $(python --version) is now active."
     # '';
 
-    myScript.text = pkgs.lib.mkForce ''
-      echo "Setting up Python with pyenv..." >&2
-
-      export PYENV_ROOT="$HOME/.pyenv"
-      export PATH="$PYENV_ROOT/bin:$PATH"
-      eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
-
-      # Get latest Python 3 version
-      version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
-
-      if [[ -z "$version" ]]; then
-        echo "Failed to determine the latest Python version." >&2
-        exit 1
-      fi
-
-      echo "Installing Python $version..." >&2
-      ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
-      ${pkgs.pyenv}/bin/pyenv global "$version"
-
-      echo "Python $(python --version) is now active."
-
-    '';
-
-    # applications.text =
-    #   let
-    #     env = pkgs.buildEnv {
-    #       name = "system-applications";
-    #       paths = config.environment.systemPackages;
-    #       pathsToLink = "/Applications";
-    #     };
-    #   in
-    #   pkgs.lib.mkForce ''
-    #       # Set up applications.
-    #       # echo "setting up /Applications..." >&2
-    #       # rm -rf /Applications/Nix\ Apps
-    #       # mkdir -p /Applications/Nix\ Apps
-    #       # find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-    #       # while read -r src; do
-    #       #   app_name=$(basename "$src")
-    #       #   echo "copying $src" >&2
-    #       #   ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-    #       # done
+    # myScript.text = pkgs.lib.mkForce ''
+    #   echo "Setting up Python with pyenv..." >&2
     #
-    #     echo "Setting up Python with pyenv..." >&2
+    #   export PYENV_ROOT="$HOME/.pyenv"
+    #   export PATH="$PYENV_ROOT/bin:$PATH"
+    #   eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
     #
-    #     export PYENV_ROOT="$HOME/.pyenv"
-    #     export PATH="$PYENV_ROOT/bin:$PATH"
-    #     eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
+    #   # Get latest Python 3 version
+    #   version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
     #
-    #     # Get latest Python 3 version
-    #     version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
+    #   if [[ -z "$version" ]]; then
+    #     echo "Failed to determine the latest Python version." >&2
+    #     exit 1
+    #   fi
     #
-    #     if [[ -z "$version" ]]; then
-    #       echo "Failed to determine the latest Python version." >&2
-    #       exit 1
-    #     fi
+    #   echo "Installing Python $version..." >&2
+    #   ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
+    #   ${pkgs.pyenv}/bin/pyenv global "$version"
     #
-    #     echo "Installing Python $version..." >&2
-    #     ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
-    #     ${pkgs.pyenv}/bin/pyenv global "$version"
+    #   echo "Python $(python --version) is now active."
     #
-    #     echo "Python $(python --version) is now active."
-    #
-    #   '';
+    # '';
+
+    applications.text =
+      let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+      pkgs.lib.mkForce ''
+        # Set up applications.
+        # echo "setting up /Applications..." >&2
+        # rm -rf /Applications/Nix\ Apps
+        # mkdir -p /Applications/Nix\ Apps
+        # find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        # while read -r src; do
+        #   app_name=$(basename "$src")
+        #   echo "copying $src" >&2
+        #   ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        # done
+
+      '';
 
   };
 }
