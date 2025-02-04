a0daac64d7526acfdfee1446c536d6720148f0c3
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-04 14:41:26 -0500

    feat: wip

--- modules/darwin/scripts.nix
+++ modules/darwin/scripts.nix
@@ -24,31 +24,32 @@
     # '';
 
     idk.text = ''
+      # Set up idk.
       echo "Setting up Python with pyenv..." >&2
       touch ~/.dotfiles/idk
 
     '';
 
-    # applications.text =
-    #   let
-    #     env = pkgs.buildEnv {
-    #       name = "system-applications";
-    #       paths = config.environment.systemPackages;
-    #       pathsToLink = "/Applications";
-    #     };
-    #   in
-    #   pkgs.lib.mkForce ''
-    #     # Set up applications.
-    #     echo "setting up /Applications..." >&2
-    #     rm -rf /Applications/Nix\ Apps
-    #     mkdir -p /Applications/Nix\ Apps
-    #     find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-    #     while read -r src; do
-    #       app_name=$(basename "$src")
-    #       echo "copying $src" >&2
-    #       ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-    #     done
-    #   '';
+    applications.text =
+      let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+      pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
   };
 }
 
