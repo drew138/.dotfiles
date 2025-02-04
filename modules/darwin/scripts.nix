5ce0c1cc86d9cd222b9e6d7de79c890a115c2f36
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-04 15:36:43 -0500

    feat: wip

--- modules/darwin/scripts.nix
+++ modules/darwin/scripts.nix
@@ -69,5 +69,27 @@
       '';
 
   };
+  system.activationScripts.languages.text = ''
+    #   echo "Setting up Python with pyenv..." >&2
+
+      export PYENV_ROOT="$HOME/.pyenv"
+      export PATH="$PYENV_ROOT/bin:$PATH"
+      eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
+
+      # Get latest Python 3 version
+      version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
+
+      if [[ -z "$version" ]]; then
+        echo "Failed to determine the latest Python version." >&2
+        exit 1
+      fi
+
+      echo "Installing Python $version..." >&2
+      ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
+      ${pkgs.pyenv}/bin/pyenv global "$version"
+
+      echo "Python $(python --version) is now active."
+
+  '';
 }
 
