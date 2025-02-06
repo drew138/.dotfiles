a088cf35530132062f4a0d484f36a075772d4a26
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-05 21:06:41 -0500

    wipo

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,8 +1,20 @@
 { ... }:
 {
-  home-manager.users.drew = { config, pkgs, ... }@args:
+  home-manager.users.drew = { config, pkgs, ... }:
     {
-      home.file = import ./symlink.nix { config = args.config; };
-      home.activation = import ./scripts.nix { pkgs = args.pkgs; };
+      # home.file = import ./symlink.nix { config = args.config; };
+      home.file =
+        {
+          ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.zshrc";
+          ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.zshrc";
+          ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.gitconfig";
+          ".gitignore_global".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.gitignore_global";
+          ".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.wezterm.lua";
+          ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/starship.toml";
+          ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/roles/nvim/files";
+        };
+      # home.activation = import ./scripts.nix {
+      #   pkgs = args.pkgs;
+      # };
     };
 }
