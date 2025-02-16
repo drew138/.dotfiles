9e90557153a50e19521b0db08057151711730d5d
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-16 17:44:11 -0500

    feat: wip

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -2,53 +2,23 @@
 {
   home-manager.users.drew = { config, pkgs, ... }:
     {
-      home.file = import ./symlink.nix { inherit config; };
-      # home.activation = import ./scripts.nix { inherit pkgs; };
-      home.activation = {
-        createFiles = ''
-          echo "Creating .workrc.zsh" >&2
-          touch ~/.workrc.zsh
-
-          echo "Creating .workrc.gitconfig" >&2
-          touch ~/.workrc.gitconfig
-        '';
-        createDirectories = ''
-          echo "Creating dev directory" >&2
-          mkdir -p ~/dev
-
-          echo "Creating work directory" >&2
-          mkdir -p ~/work
-        '';
-
-        installPython = ''
-          echo "Setting up Python with pyenv..." >&2
-
-          export PYENV_ROOT="$HOME/.pyenv"
-          export PATH="$PYENV_ROOT/bin:$PATH"
-          eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
-
-          # Get latest Python 3 version
-          version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
-
-          if [[ -z "$version" ]]; then
-            echo "Failed to determine the latest Python version." >&2
-            exit 1
-          fi
-
-          echo "Installing Python $version..." >&2
-          ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
-          ${pkgs.pyenv}/bin/pyenv global "$version"
-
-          echo "Python $(python --version) is now active."
-        '';
-
-        installNode = ''
-          echo "Setting up Node with fnm..." >&2
-
-          ${pkgs.fnm}/bin/fnm install --latest
-          ${pkgs.fnm}/bin/fnm use --latest
-          ${pkgs.fnm}/bin/fnm default $(${pkgs.fnm}/bin/fnm current)
-        '';
+      # home.file = import ./symlink.nix { inherit config; };
+      home.file =
+        let
+          symlink = config.lib.file.mkOutOfStoreSymlink;
+          homeDirectory = config.home.homeDirectory;
+        in
+        {
+          ".zshrc".source = symlink "${homeDirectory}/.dotfiles/.zshrc";
+          ".tmux.conf".source = symlink "${homeDirectory}/.dotfiles/.zshrc";
+          ".gitconfig".source = symlink "${homeDirectory}/.dotfiles/.gitconfig";
+          ".gitignore_global".source = symlink "${homeDirectory}/.dotfiles/.gitignore_global";
+          ".wezterm.lua".source = symlink "${homeDirectory}/.dotfiles/.wezterm.lua";
+          ".config/starship.toml".source = symlink "${homeDirectory}/.dotfiles/starship.toml";
+          ".config/nvim".source = symlink "${homeDirectory}/.dotfiles/roles/nvim/files";
+        };
+      home.activation = import ./scripts.nix {
+        inherit pkgs;
       };
     };
 }
