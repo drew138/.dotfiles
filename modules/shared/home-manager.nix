39e4e46da82b6fca905aed4441a45540ecea20dc
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-17 08:54:40 -0500

    feat: wip

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -2,23 +2,7 @@
 {
   home-manager.users.drew = { config, pkgs, ... }:
     {
-      # home.file = import ./symlink.nix { inherit config; };
-      home.file =
-        let
-          symlink = config.lib.file.mkOutOfStoreSymlink;
-          homeDirectory = config.home.homeDirectory;
-        in
-        {
-          ".zshrc".source = symlink "${homeDirectory}/.dotfiles/.zshrc";
-          ".tmux.conf".source = symlink "${homeDirectory}/.dotfiles/.zshrc";
-          ".gitconfig".source = symlink "${homeDirectory}/.dotfiles/.gitconfig";
-          ".gitignore_global".source = symlink "${homeDirectory}/.dotfiles/.gitignore_global";
-          ".wezterm.lua".source = symlink "${homeDirectory}/.dotfiles/.wezterm.lua";
-          ".config/starship.toml".source = symlink "${homeDirectory}/.dotfiles/starship.toml";
-          ".config/nvim".source = symlink "${homeDirectory}/.dotfiles/roles/nvim/files";
-        };
-      home.activation = import ./scripts.nix {
-        inherit pkgs;
-      };
+      home.file = import ./symlink.nix { inherit config; };
+      home.activation = import ./scripts.nix { inherit pkgs; };
     };
 }
