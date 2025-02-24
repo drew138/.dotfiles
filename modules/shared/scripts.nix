e658e25cdfcc49c9df3ef0066ebbe4410aa16a2a
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-23 20:23:21 -0500

    feat: wip

--- modules/shared/scripts.nix
+++ modules/shared/scripts.nix
@@ -39,13 +39,13 @@
   installNode = ''
     echo "Setting up Node with fnm..." >&2
 
-    eval "$(${pkgs.fnm}/bin/fnm env)"
 
     version=$(${pkgs.fnm}/bin/fnm ls-remote --lts | tail -n 1 | awk '{print $1}')
 
     ${pkgs.fnm}/bin/fnm install $version
     ${pkgs.fnm}/bin/fnm use $version
   '';
+  # eval "$(${pkgs.fnm}/bin/fnm env)"
   # ${pkgs.fnm}/bin/fnm default $(${pkgs.fnm}/bin/fnm current)
 
   installRust = ''
