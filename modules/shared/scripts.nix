e3517d23632a28adc2c538c292efaa6872bc9e22
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-27 20:50:44 -0500

    feat: wip

--- modules/shared/scripts.nix
+++ modules/shared/scripts.nix
@@ -14,38 +14,39 @@
     mkdir -p ~/work
   '';
 
-  # installPython = ''
-  #   echo "Setting up Python with pyenv..." >&2
-  #
-  #   # export PYENV_ROOT="$HOME/.pyenv"
-  #   # export PATH="$PYENV_ROOT/bin:$PATH"
-  #   # eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
-  #
-  #   # Get latest Python 3 version
-  #   version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
-  #
-  #   if [[ -z "$version" ]]; then
-  #     echo "Failed to determine the latest Python version." >&2
-  #     exit 1
-  #   fi
-  #
-  #   echo "Installing Python $version..." >&2
-  #   ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
-  #   ${pkgs.pyenv}/bin/pyenv global "$version"
-  #
-  #   echo "Python $(python3 --version) is now active." >&2
-  # '';
+  installPython = ''
+    echo "Setting up Python with pyenv..." >&2
 
-  installNode = ''
-    echo "Setting up Node with fnm..." >&2
+    # Get latest Python 3 version
+    version=$(${pkgs.pyenv}/bin/pyenv install --list | grep " 3\.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' ')
 
-    eval "$(${pkgs.fnm}/bin/fnm env)"
+    if [[ -z "$version" ]]; then
+      echo "Failed to determine the latest Python version." >&2
+      exit 1
+    fi
 
-    version=$(${pkgs.fnm}/bin/fnm ls-remote --lts | tail -n 1 | /usr/bin/awk '{print $1}')
+    echo "Installing Python $version..." >&2
+    ${pkgs.pyenv}/bin/pyenv install --skip-existing "$version"
+    ${pkgs.pyenv}/bin/pyenv global "$version"
 
-    ${pkgs.fnm}/bin/fnm install $version
-    ${pkgs.fnm}/bin/fnm use $version
+    echo "Python $(python3 --version) is now active." >&2
   '';
+
+  # export PYENV_ROOT="$HOME/.pyenv"
+  # export PATH="$PYENV_ROOT/bin:$PATH"
+  # eval "$(${pkgs.pyenv}/bin/pyenv init --path)"
+
+
+  # installNode = ''
+  #   echo "Setting up Node with fnm..." >&2
+  #
+  #   eval "$(${pkgs.fnm}/bin/fnm env)"
+  #
+  #   version=$(${pkgs.fnm}/bin/fnm ls-remote --lts | tail -n 1 | /usr/bin/awk '{print $1}')
+  #
+  #   ${pkgs.fnm}/bin/fnm install $version
+  #   ${pkgs.fnm}/bin/fnm use $version
+  # '';
   # ${pkgs.fnm}/bin/fnm default $(${pkgs.fnm}/bin/fnm current)
 
   installRust = ''
