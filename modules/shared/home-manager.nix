2c084d1701f5c47d996442b677627e43cc571126
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-02-28 11:17:25 -0500

    feat: wip

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -3,6 +3,6 @@
   home-manager.users.drew = { config, pkgs, ... }:
     {
       home.file = import ./symlink.nix { inherit config; };
-      # home.activation = import ./scripts.nix { inherit pkgs; };
+      home.activation = import ./scripts.nix { inherit pkgs; };
     };
 }
