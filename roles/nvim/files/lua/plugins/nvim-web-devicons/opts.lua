ae88b7193746923f41cc3cecabb924c62b689c13
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2025-12-25 09:22:03 -0500

    feat: fix

--- roles/nvim/files/lua/plugins/nvim-web-devicons/opts.lua
+++ roles/nvim/files/lua/plugins/nvim-web-devicons/opts.lua
@@ -11,6 +11,23 @@ local M = {
 			cterm_color = "68",
 			name = "Dockerfile",
 		},
+		["sketchybarrc"] = {
+			icon = "",
+			color = "#89e051",
+			cterm_color = "113",
+			name = "sketchybarrc",
+		},
+        sh = {
+            icon = "",
+            color = "#89e051",
+            cterm_color = "113",
+            name = "Shell",
+        },
+        [".yamllint"] = {
+            icon = "",
+            color = "#6d8086",
+            name = "Yaml",
+        },
 	},
 }
 
--- roles/nvim/files/lua/user/filetype.lua
+++ roles/nvim/files/lua/user/filetype.lua
@@ -1,9 +1,14 @@
 vim.filetype.add({
+    filename = {
+        [".yamllint"] = "yaml",
+        sketchybarrc = "bash",
+    },
 	extension = {
 		gitconfig = "gitconfig",
 	},
 	pattern = {
 		["[Dd]ockerfile.*"] = "dockerfile",
 		[".gitignore.*"] = "gitignore",
+        ["sketchybarrc"] = "bash",
 	},
 })
